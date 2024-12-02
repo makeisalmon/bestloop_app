d49e0f4e0e43899ee8b3b6132b8f29e6c688bbcd
JordanCarden <154265139+JordanCarden@users.noreply.github.com>
2024-12-01 20:28:29 -0600

    Added sign in pages

---
+++ lib/pages/sign_in2.dart
@@ -0,0 +1,109 @@
+import 'package:bestloop_app/main.dart';
+import 'package:flutter/material.dart';
+import 'package:bestloop_app/shared.dart';
+import 'package:bestloop_app/pages/sign_in.dart';
+import 'package:bestloop_app/pages/leaderboard_page.dart';
+
+class SignIn2 extends StatelessWidget {
+  const SignIn2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+
+      backgroundColor: Colors.black,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 60),
+            Text(
+              'Sign in',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 36,
+                fontWeight: FontWeight.normal,
+              ),
+            ),
+            const SizedBox(height: 40),
+            // Email TextField
+            TextField(
+              obscureText: false,
+              style: TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                hintText: 'Email',
+                hintStyle: TextStyle(color: Colors.grey),
+                filled: true,
+                fillColor: Colors.grey[850],
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5),
+                  borderSide: BorderSide.none,
+                ),
+                contentPadding: EdgeInsets.symmetric(vertical:10, horizontal: 12)
+              ),
+            ),
+            const SizedBox(height: 40),
+            // Password TextField
+            TextField(
+              obscureText: true,
+              style: TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                hintText: 'Password',
+                hintStyle: TextStyle(color: Colors.grey),
+                filled: true,
+                fillColor: Colors.grey[850],
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5),
+                  borderSide: BorderSide.none,
+                ),
+                contentPadding: EdgeInsets.symmetric(vertical:10, horizontal: 12)
+              ),
+            ),
+            const SizedBox(height: 20),
+            // Confirm Password TextField
+            TextField(
+              obscureText: true,
+              style: TextStyle(color: Colors.white),
+              decoration: InputDecoration(
+                hintText: 'Confirm password',
+                hintStyle: TextStyle(color: Colors.grey),
+                filled: true,
+                fillColor: Colors.grey[850],
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5),
+                  borderSide: BorderSide.none,
+                ),
+                contentPadding: EdgeInsets.symmetric(vertical:10, horizontal: 12)
+              ),
+            ),
+            const SizedBox(height: 20),
+            // Continue Button
+
+            BestLoopButton(
+              text: "Continue",
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const GlobalScaffold(title: 'help'),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
